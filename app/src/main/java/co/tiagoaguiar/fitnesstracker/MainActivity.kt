f4599b4e267ec86a2907163373dc3df7a51ad082
kimMichel <kmichel.jae@gmail.com>
2022-07-12 00:56:17 -0300

    change url repository, add intent and activity

--- app/src/main/java/co/tiagoaguiar/fitnesstracker/MainActivity.kt
+++ app/src/main/java/co/tiagoaguiar/fitnesstracker/MainActivity.kt
@@ -1,11 +1,24 @@
 package co.tiagoaguiar.fitnesstracker
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.LinearLayout
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var btnImc: LinearLayout
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        btnImc = findViewById(R.id.btn_imc)
+        btnImc.setOnClickListener {
+            // Navegar para a próxima tela
+            val i = Intent(this, ImcActivity::class.java) // Criei a inteção de navegar para outra tela
+            startActivity(i) // Começar uma atividade (a intenção)
+        }
+
     }
 }
