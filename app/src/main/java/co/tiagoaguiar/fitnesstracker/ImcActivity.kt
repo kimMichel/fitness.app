b1c63ccba08e4c55203d6488a3b64a2d71e1f734
kimMichel <kmichel.jae@gmail.com>
2022-07-13 22:41:09 -0300

    implement imc calc method, add dialog

--- app/src/main/java/co/tiagoaguiar/fitnesstracker/ImcActivity.kt
+++ app/src/main/java/co/tiagoaguiar/fitnesstracker/ImcActivity.kt
@@ -1,10 +1,15 @@
 package co.tiagoaguiar.fitnesstracker
 
+import android.content.DialogInterface
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
 import android.widget.EditText
 import android.widget.Toast
+import androidx.annotation.StringRes
+import androidx.appcompat.app.AlertDialog
 
 class ImcActivity : AppCompatActivity() {
 
@@ -28,8 +33,42 @@ class ImcActivity : AppCompatActivity() {
                 // se retornar sem o @setOnClickListener vai matar a execução do onCreate
                 return@setOnClickListener
             }
+            val weight = editWeight.text.toString().toInt()
+            val height = editHeight.text.toString().toInt()
 
+            val result = imcCalculate(weight, height)
+            // Log.i("test", "resultado: $result") dica: utilize o log para verificar se a lógica está funcionando corretamente
+
+            val dialog = AlertDialog.Builder(this) // código padrão para criar "dialog"
+
+                .setTitle(getString(R.string.imc_response, result))
+                .setMessage(imcResponse(result))
+                .setPositiveButton(android.R.string.ok) { dialog, which ->
+                    // aqui vai rodar depois do click
+                    val i = Intent(this, MainActivity::class.java)
+                    startActivity(i)
+                }
+                .create()
+                .show()
         }
+    }
+
+    @StringRes //Essa notação força ao usuário retornar um arquivo de resource
+    private fun imcResponse(imc: Double): Int {
+        return when {
+            imc < 15.0 -> R.string.imc_severely_low_weight
+            imc < 16.0 -> R.string.imc_very_low_weight
+            imc < 18.5 -> R.string.imc_low_weight
+            imc < 25.0 -> R.string.normal
+            imc < 30.0 -> R.string.imc_high_weight
+            imc < 35.0 -> R.string.imc_so_high_weight
+            imc < 40.0 -> R.string.imc_severely_high_weight
+            else -> R.string.imc_extreme_weight
+        }
+    }
+
+    private fun imcCalculate(weight: Int, height: Int): Double {
+        return weight / ((height / 100.0) * (height / 100.0))
     }
 
     private fun validate(): Boolean {
